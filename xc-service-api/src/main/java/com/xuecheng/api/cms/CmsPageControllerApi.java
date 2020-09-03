9f7a4a180f7c267428bee7398f28b387edfb17a5
harsh <hmc0422@163.com>
2020-09-03 19:13:51 +0800

    Revert "CmsPage新增，修改，删除"
    This reverts commit 977a77dc0be377364b7d60967b1f2d3a75fe4ba3.

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -1,10 +1,7 @@
 package com.xuecheng.api.cms;
 
-import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
-import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.QueryResponseResult;
-import com.xuecheng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -25,41 +22,4 @@ public interface CmsPageControllerApi {
     @ApiImplicitParams({@ApiImplicitParam(name = "page", value = "页码", required = true, paramType = "path", dataType = "int"),
             @ApiImplicitParam(name = "size", value = "每页记录数", required = true, paramType = "path", dataType = "int")})
     public QueryResponseResult findPageList(int page, int size, QueryPageRequest queryPageRequest);
-
-    /**
-     * 添加页面
-     *
-     * @param cmsPage
-     * @return
-     */
-    @ApiOperation("添加页面")
-    public CmsPageResult addCmsPageInfo(CmsPage cmsPage);
-
-    /**
-     * 通过id查找
-     *
-     * @param id
-     * @return
-     */
-    @ApiOperation("通过id查找页面")
-    public CmsPage findCmsPageInfoById(String id);
-
-    /**
-     * 修改页面
-     *
-     * @param cmsPage
-     * @return
-     */
-    @ApiOperation("修改页面")
-    public CmsPageResult updateCmsPageInfo(String id, CmsPage cmsPage);
-
-    /**
-     * 根据ID删除页面
-     *
-     * @param id
-     * @return
-     */
-    @ApiOperation("根据ID删除页面")
-    public ResponseResult delCmsPageInfo(String id);
-
 }
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -1,14 +1,14 @@
 package com.xuecheng.manage_cms.controller;
 
 import com.xuecheng.api.cms.CmsPageControllerApi;
-import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
-import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.QueryResponseResult;
-import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_cms.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cms/page")
@@ -21,29 +21,5 @@ public class CmsPageController implements CmsPageControllerApi {
     @GetMapping("/list/{page}/{size}")
     public QueryResponseResult findPageList(@PathVariable("page") int page, @PathVariable("size") int size, QueryPageRequest queryPageRequest) {
         return pageService.findPageList(page, size, queryPageRequest);
-    }
-
-    @Override
-    @PostMapping("/add")
-    public CmsPageResult addCmsPageInfo(@RequestBody CmsPage cmsPage) {
-        return pageService.addCmsPageInfo(cmsPage);
-    }
-
-    @Override
-    @GetMapping("/get/{id}")
-    public CmsPage findCmsPageInfoById(@PathVariable("id") String id) {
-        return pageService.findCmsPageInfoById(id);
-    }
-
-    @Override
-    @PutMapping("/edit/{id}")
-    public CmsPageResult updateCmsPageInfo(@PathVariable("id") String id, @RequestBody CmsPage cmsPage) {
-        return pageService.updateCmsPageInfo(id, cmsPage);
-    }
-
-    @Override
-    @DeleteMapping("/del/{id}")
-    public ResponseResult delCmsPageInfo(@PathVariable("id") String id) {
-        return pageService.delCmsPageInfo(id);
     }
 }
